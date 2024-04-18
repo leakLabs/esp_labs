7df8b92b435be64983ab00f245834111a4a4ddb2
leakLabs <oko48ru@mail.ru>
2024-04-18 19:31:44 +0300

    lab2 f

--- lab2/main.c
+++ lab2/main.c
@@ -1,11 +1,40 @@
-#define F_CPU 12000000
-#define __AVR_ATtiny84__ 
-#define BUTTON1_PIN PA0
-#define BUTTON2_PIN PA1
-#define SWITCH_PIN  PA2
-#define LED1_PIN PB0
-#define LED2_PIN PB1
-#define LED3_PIN PB2
+// #define F_CPU 12000000
+// #define __AVR_ATtiny84__ 
+
+// #define LED1_PORT 0
+// #define LED1_PIN 0
+// #define LED2_PORT 0
+// #define LED2_PIN 0
+// #define LEDOUT_PORT 0
+// #define LEDOUT_PIN 0
+// #define B1_PORT 0
+// #define B1_PIN 0
+// #define B2_PORT 0
+// #define B2_PIN 0
+// #define SWITCH_PORT 0
+// #define SWITCH_PIN 0
+// #define DDR_L1 0
+// #define PORT_L1 0
+// #define PIN_L1 0
+// #define DDR_L2 0
+// #define PORT_L2 0
+// #define PIN_L2 0
+// #define DDR_LEDOUT 0
+// #define PORT_LEDOUT 0
+// #define PIN_LEDOUT 0
+// #define DDR_B1 0
+// #define PORT_B1 0
+// #define PIN_B1 0
+// #define DDR_B2 0
+// #define PORT_B2 0
+// #define PIN_B2 0
+// #define DDR_SWITCH 0
+// #define PORT_SWITCH 0
+// #define PIN_SWITCH 0
+// #define PCIE_B1 0
+// #define PCIE_B2 0
+// #define PCMSK_B1 0
+// #define PCMSK_B2 0
 
 #include <avr/io.h>
 #include <util/delay.h>
@@ -16,13 +45,17 @@ volatile uint8_t button2State = 0;
 
 void setup();
 
-ISR(PCINT0_vect) {
-    if(!(PINA & (1 << SWITCH_PIN))) {
-        if (!(PINA & (1 << BUTTON1_PIN))) {
+#if defined(__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
+ISR(PCINT1_vect, ISR_ALIASOF(PCINT0_vect));
+#endif
+ISR(PCINT0_vect)
+{
+    if(!(PIN_SWITCH & (1 << SWITCH_PIN))) {
+        if (!(PIN_B1 & (1 << B1_PIN))) {
             button1State = ~button1State;
         }
 
-        if (!(PINA & (1 << BUTTON2_PIN))) {
+        if (!(PIN_B2 & (1 << B2_PIN))) {
             button2State = ~button2State;
         }
     }
@@ -31,23 +64,40 @@ ISR(PCINT0_vect) {
 int main(void) {
     setup();
     while(1) {
-        if (!(PINA & (1 << SWITCH_PIN))) {
-            PORTB = ((1 << LED1_PIN) & button1State | ~(1 << LED1_PIN) & PORTB);
-            PORTB = ((1 << LED2_PIN) & button2State | ~(1 << LED2_PIN) & PORTB);
-            PORTB = ((1 << LED3_PIN) & (button1State ^ button2State) | ~(1 << LED3_PIN) & PORTB);
+        if (!(PIN_SWITCH & (1 << SWITCH_PIN))) {
+            PORT_L1 = ((1 << LED1_PIN) & button1State | ~(1 << LED1_PIN) & PORT_L1);
+            PORT_L2 = ((1 << LED2_PIN) & button2State | ~(1 << LED2_PIN) & PORT_L2);
+            PORT_LEDOUT = ((1 << LEDOUT_PIN) & (button1State ^ button2State) | ~(1 << LEDOUT_PIN) & PORT_LEDOUT);
         } else {
-            PORTB &= ~((1 << LED1_PIN) | (1 << LED2_PIN) | (1 << LED3_PIN));
+            //PORTB &= ~((1 << LED1_PIN) | (1 << LED2_PIN) | (1 << LED3_PIN));
+            PORT_L1 &= ~(1 << LED1_PIN);
+            PORT_L2 &= ~(1 << LED2_PIN);
+            PORT_LEDOUT &= ~(1 << LEDOUT_PIN);
         }
     }
 }
 
 void setup() {
-    DDRB |= (1 << LED1_PIN) | (1 << LED2_PIN) | (1 << LED3_PIN);
-    DDRA &= ~((1 << BUTTON1_PIN) | (1 << BUTTON2_PIN) | (1 << SWITCH_PIN));
+    //DDRB |= (1 << LED1_PIN) | (1 << LED2_PIN) | (1 << LED3_PIN);
+    DDR_L1 |= (1 << LED1_PIN);
+    DDR_L2 |= (1 << LED2_PIN);
+    DDR_LEDOUT |= (1 << LEDOUT_PIN);
 
-    PORTA |= (1 << BUTTON1_PIN) | (1 << BUTTON2_PIN) | (1 << SWITCH_PIN);
+    //DDRA &= ~((1 << BUTTON1_PIN) | (1 << BUTTON2_PIN) | (1 << SWITCH_PIN));
+    DDR_B1 &= ~(1 << B1_PIN);
+    DDR_B2 &= ~(1 << B2_PIN);
+    DDR_SWITCH &= ~(1 << SWITCH_PIN);
 
-    GIMSK |= (1 << PCIE0);
-    PCMSK0 |= (1 << PCINT1) | (1 << PCINT0);
+    //PORTA |= (1 << BUTTON1_PIN) | (1 << BUTTON2_PIN) | (1 << SWITCH_PIN);
+    PORT_B1 |= (1 << B1_PIN);
+    PORT_B2 |= (1 << B2_PIN);
+    PORT_SWITCH |= (1 << SWITCH_PIN);
+
+    GIMSK |= (1 << PCIE_B1);
+    GIMSK |= (1 << PCIE_B2);
+
+    //PCMSK0 |= (1 << PCINT1) | (1 << PCINT0);
+    PCMSK_B1 |= (1 << B1_PIN);
+    PCMSK_B2 |= (1 << B2_PIN);
     sei();
 }
